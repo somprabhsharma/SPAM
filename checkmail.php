9e6736a5d0a8be471530ac5c9708334a1d957003
somprabhsharma <somprabhsharma@outlook.com>
2015-10-30 11:41:28 +0530

    Update 1
    update

--- checkmail.php
+++ checkmail.php
@@ -9,18 +9,18 @@ require ('class.smtp.php');
 $mail = new PHPMailer();
 
 $mail->isSMTP();                    
-$mail->Host = 'p3plcpnl0912.prod.phx3.secureserver.net';  // Specify main and backup server
+$mail->Host ='hostnameurl';  // Specify main and backup server
 //$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
     $mail->SMTPAuth   = true;                            // Enable SMTP authentication
-$mail->Username = 'support@somprabhsharma.com';                            // SMTP username
-$mail->Password = 'war5625som';                           // SMTP password
+$mail->Username = 'email@email.com';                            // SMTP username
+$mail->Password = 'password';                           // SMTP password
 $mail->SMTPSecure = 'ssl';  
 $mail->Port = 465;
 
 
-$mail->From = 'support@somprabhsharma.com';
+$mail->From = 'email@email.com';
 $mail->FromName = 'som';
-$mail->addAddress('thewarlocksom@gmail.com');  // Add a recipient
+$mail->addAddress('email@email.com');  // Add a recipient
 
 $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
 
